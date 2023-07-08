c9cb117aebf86f4bcb634934ae9d268825abbe9f
Phraephan Chaiyato <Anne@Annes-MacBook-Pro.local>
2023-07-07 18:07:28 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// let confetti_animed = false;
+// document.querySelector(".confetti-wrapper").addEventListener("mouseover", () => {
+//     var subTitle = document.getElementById("sub-title");
+//     if(!confetti_animed) {
+//         let canvas = document.createElement("canvas");
+//         let container = document.getElementsByClassName("confetti-wrapper")[0];
+//         canvas.width = 2000;
+//         canvas.height = 2000;
+//         container.appendChild(canvas);
+//         let confetti_heading = confetti.create(canvas);
+//         confetti_heading({
+//             particleCount: 200,
+//             spread: 80,
+//             startVelocity: 30,
+//         }).then( () => {
+            
+//             subTitle.classList.add("removed");
+            
+//             subTitle.remove();
+            
+//             container.removeChild(canvas);
+            
+//         }
+//         );
+//         confetti_animed = true;
+//     }
+    
+// });
+
+var typed = new Typed('#sub-title', {
+    strings: ['ANNE CHAIYATO'],
+    typeSpeed: 90,
+  });
+
+  document.getElementById('newsletter-card').addEventListener("click", () => {
+
+  });
+
+
+
+    
+
